10245efa132e1ec09f00441a83cbca3c2420549d
BahaleCodes <74527555+BahaleCodes@users.noreply.github.com>
2020-11-22 23:18:26 +0200

    update

--- src/Containers/Auth/Auth.js
+++ src/Containers/Auth/Auth.js
@@ -27,17 +27,17 @@ function Auth() {
       const isDisabled = username.length === 0 || password.length === 0;
     
       return (
-        <div className="App">
+        <div className="Auth">
           <header>
             {isLoginView ? <h1>Login</h1> : <h1>Register</h1>}
           </header>
           <div className="login-container">
             <label htmlFor="username">Username</label><br/>
-            <input id="username" type="text" placeholder="username" value={username}
+            <input id="username" type="text" placeholder="Username" value={username}
                   onChange={ evt=> setUsername(evt.target.value)}
             /><br/>
             <label htmlFor="password">Password</label><br/>
-            <input id="password" type="password" placeholder="Descriptiom" value={password}
+            <input id="password" type="password" placeholder="Password" value={password}
                 onChange={ evt=> setPassword(evt.target.value)} /><br/>
                 { isLoginView ?
                   <button onClick={loginClicked} disabled={isDisabled}>Login</button> : 
